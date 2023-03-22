408535c3eb812e1266464442151736aa41bde9db
Leandro G. da Cruz <77545660+delt4d@users.noreply.github.com>
2023-03-21 23:18:12 -0300

    Change assets location

--- src/components/ProductImage.tsx
+++ src/components/ProductImage.tsx
@@ -1,10 +1,9 @@
-import { useEffect, useState } from "react";
 import { useImageImport } from "../hooks/image-import"
 
 type ProductImageProps = React.HTMLProps<HTMLImageElement> & { src: string };
 
 export function ProductImage(props: ProductImageProps) {
-    const image = useImageImport(`../assets/img/produtos/${props.src}.png`);
+    const image = useImageImport(`/assets/img/produtos/${props.src}.png`);
 
     return (
         <img {...props} src={image} />
